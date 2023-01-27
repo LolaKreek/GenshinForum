fa2d16308b0f483a958ca64156f19e8b735fdb62
Lalita <lalitaklimchuk@gmail.com>
2023-01-27 11:12:41 +0100

    Added styles for comments. Added functionality for adding comments

--- src/components/CommentSection/CommentSection.js
+++ src/components/CommentSection/CommentSection.js
@@ -1,11 +1,11 @@
 import React, { useContext, useRef, useState } from 'react';
 import { auth, db, storage } from "../../firebase";
 import {  onAuthStateChanged } from "firebase/auth";
-import { Link } from 'react-router-dom'
-import { Button, Container } from '../../globalStyles'
-import './../../styles/userPage.css';
+import { Link } from 'react-router-dom';
+import { Button, Container } from '../../globalStyles';
 import { AuthContext } from '../../context/AuthContext';
 import Loader from "../Loader/Loader";
+import "../../styles/commSec.css";
 
 import {doc, serverTimestamp,setDoc, Timestamp } from "firebase/firestore";
 import { collection, query, where, getDocs, getDoc } from "firebase/firestore";
@@ -29,6 +29,7 @@ import {
   FormLabel,
   CommentsWrapper
 } from './CommentSection.elements'
+import NewComment from '../NewComment/NewComment';
 
 
 const CommentSection = () => {
@@ -40,30 +41,32 @@ const CommentSection = () => {
     const [personalData, setPersonalData] = useState({});
     const [commentImage, setCommentImageLink] = useState("");
     const [commentData, setCommentData] = useState({});
+    const [nameComment, setNameComment] = useState('');
+    const [isNewComment, setIsNewComment] = useState(false);
 
 
     
-    const getUser = async (e) => {
-    window.scrollTo(0, 0);
-    setifLoadPage(true);
-    const docRef = doc(db, "users", currentUser.uid);
-    console.log("handldeAdd CommSec : " + docRef);
-    const querySnapshot = await getDoc(docRef);
-    if (querySnapshot.exists()) {
-      setPersonalData(doc.data());
-      setImageLink(doc.data().image);
-    }
-    setifLoadPage(false);
-    }
+    // const getUser = async (e) => {
+    // window.scrollTo(0, 0);
+    // setifLoadPage(true);
+    // const docRef = doc(db, "users", currentUser.uid);
+    // const querySnapshot = await getDoc(docRef);
+    // if (querySnapshot.exists()) {
+    //   setPersonalData(doc.data());
+    //   setImageLink(doc.data().image);
+    // }
+    // setifLoadPage(false);
+    // }
 
     const handleAdd = async (e) => {
-        getUser();
+        // getUser();
         e.preventDefault();
-        setifLoadPage(true);
+        setNameComment(e.target[0].value);
+        setCommentData(e.target[1].value);
+        setIsNewComment(true);
+        // setifLoadPage(true);
 
-        try{
-            
-            
+        try{            
             await setDoc(doc(db, "comments"), {
                 comment: commentContent,
                 //userName: personalData.name,
@@ -71,7 +74,7 @@ const CommentSection = () => {
                 userID: currentUser.uid,
               });
               alert("Comment added successfully");
-              setifLoadPage(false);
+            //   setifLoadPage(false);
         } catch (err) {
             console.log("handldeAdd CommSec : " + err);
         }
@@ -79,35 +82,38 @@ const CommentSection = () => {
     }
 
     //где вызвать
-    const getComments = async (e) => {
-        const docRef = doc(db, "comments");
-        const docSnap = await getDocs(docRef);
-        docSnap.forEach((doc) => {
-            setCommentData(doc.data());
-            setCommentImageLink(doc.data().image);
-          });
-    }
+    // const getComments = async (e) => {
+    //     const docRef = doc(db, "comments");
+    //     const docSnap = await getDocs(docRef);
+    //     docSnap.forEach((doc) => {
+    //         setCommentData(doc.data());
+    //         setCommentImageLink(doc.data().image);
+    //       });
+    // }
     
   return (
     <>
         {ifLoadPage ? <Loader /> : ''}
         <InfoSec lightBg={true}>
+            <h1 className='info-sec-header'>Share your post</h1>
             <Container>
-                <FormWrapper start={true}>
+                <FormWrapper className='main-form-wrapper' start={true}>
+                    <FormLabel className='wrapper-header'><h2 className='header-h2'>Add your comment</h2></FormLabel>
                     <Form className="content__form" onSubmit={handleAdd}>
-                        <FormLabel>Make a post</FormLabel>
-                        <InputWrapper > 
+                        <InputWrapper className='input-wrapper'> 
+                            <FormInput id="comment_header" type='text' placeholder={"Enter your user name"} onChange={e => setComment(e.target.value)} />
                             <FormInput id="comment_content" type='text' placeholder={"Share your opinion"} onChange={e => setComment(e.target.value)} />
-                            <Button className="submit_btn_comm_sec" type="submit">Send</Button>
                         </InputWrapper>
+                        <Button className="submit_btn_comm_sec" type="submit">Send</Button>
                     </Form>
                 </FormWrapper>
-                <CommentsWrapper> 
+
+                {isNewComment ? <NewComment nameComment={nameComment} commentData={commentData} /> : ''}
+
+                <CommentsWrapper className='comment__main-container'> 
                     <section className="vh-100">
-                        <MDBContainer className="py-5" style={{ maxWidth: "1000px" }}>
-                            <MDBRow className="justify-content-center">
-                            <MDBCol md="11" lg="9" xl="7">
-                                <div className="d-flex flex-start mb-4">
+                        <div className="flex-container">
+                            <div className='main-image__main-container'>
                                 <img
                                     className="rounded-circle shadow-1-strong me-3"
                                     src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(32).webp"
@@ -115,31 +121,131 @@ const CommentSection = () => {
                                     width="65"
                                     height="65"
                                 />
+                            </div>
 
-                                <MDBCard className="w-100">
-                                    <MDBCardBody className="p-4">
-                                    <div>
-                                        <MDBTypography tag="h5">Johny Cash</MDBTypography>
-                                        <p className="small">3 hours ago</p>
-                                        <p id="user_comment_content">
-                                        Cras sit amet nibh libero, in gravida nulla. Nulla vel
-                                        metus scelerisque ante sollicitudin. Cras purus odio,
-                                        vestibulum in vulputate at, tempus viverra turpis. Fusce
-                                        condimentum nunc ac nisi vulputate fringilla. Donec
-                                        lacinia congue felis in faucibus ras purus odio,
-                                        vestibulum in vulputate at, tempus viverra turpis.
-                                        </p>
-
-                                    </div>
-                                    </MDBCardBody>
-                                </MDBCard>
+                            <MDBCard className="w-100 main-content-container">
+                                <div className='container-headers'>
+                                    <h1 className='main-header' >Johny Cash</h1>
+                                    <p className="small second-header">3 hours ago</p>
                                 </div>
 
-                            </MDBCol>
-                            </MDBRow>
-                        </MDBContainer>
-                        </section>
-                    
+                                <div className='container-comment-content'>
+                                    <p id="user_comment_content">
+                                    Cras sit amet nibh libero, in gravida nulla. Nulla vel
+                                    metus scelerisque ante sollicitudin. Cras purus odio,
+                                    vestibulum in vulputate at, tempus viverra turpis. Fusce
+                                    condimentum nunc ac nisi vulputate fringilla. Donec
+                                    lacinia congue felis in faucibus ras purus odio,
+                                    vestibulum in vulputate at, tempus viverra turpis.
+                                    </p>
+                                </div>
+                            </MDBCard>
+                        </div>
+                    </section>
+                </CommentsWrapper>
+
+                <CommentsWrapper className='comment__main-container'> 
+                    <section className="vh-100">
+                        <div className="flex-container">
+                            <div className='main-image__main-container'>
+                                <img
+                                    className="rounded-circle shadow-1-strong me-3"
+                                    src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+                                    alt="avatar"
+                                    width="65"
+                                    height="65"
+                                />
+                            </div>
+
+                            <MDBCard className="w-100 main-content-container">
+                                <div className='container-headers'>
+                                    <h1 className='main-header' >James Smith</h1>
+                                    <p className="small second-header">2 days ago</p>
+                                </div>
+
+                                <div className='container-comment-content'>
+                                    <p id="user_comment_content">
+                                    Kept in sent gave feel will oh it we. Has pleasure procured men 
+                                    laughing shutters nay. Old insipidity motionless continuing law 
+                                    shy partiality. Depending acuteness dependent eat use dejection. 
+                                    Unpleasing astonished discovered not nor shy. Morning hearted now 
+                                    met yet beloved evening. Has and upon his last here must. 
+                                    </p>
+                                </div>
+                            </MDBCard>
+                        </div>
+                    </section>
+                </CommentsWrapper>
+
+                <CommentsWrapper className='comment__main-container'> 
+                    <section className="vh-100">
+                        <div className="flex-container">
+                            <div className='main-image__main-container'>
+                                <img
+                                    className="rounded-circle shadow-1-strong me-3"
+                                    src="https://www.pinkvilla.com/imageresize/bts_v_leather_jacket_butter_concept_photo_main_1.jpg?width=752&t=pvorg"
+                                    alt="avatar"
+                                    width="65"
+                                    height="65"
+                                />
+                            </div>
+
+                            <MDBCard className="w-100 main-content-container">
+                                <div className='container-headers'>
+                                    <h1 className='main-header' >Kim Taehyung</h1>
+                                    <p className="small second-header">14 days ago</p>
+                                </div>
+
+                                <div className='container-comment-content'>
+                                    <p id="user_comment_content">
+                                    Possession her thoroughly remarkably terminated man continuing. 
+                                    Removed greater to do ability. You shy shall while but wrote 
+                                    marry. Call why sake has sing pure. Gay six set polite nature 
+                                    worthy. So matter be me we wisdom should basket moment merely. 
+                                    Me burst ample wrong which would mr he could. Visit arise my 
+                                    point timed drawn no. Can friendly laughter goodness man him 
+                                    appetite carriage. Any widen see gay forth alone fruit bed.&#10084;
+                                    </p>
+                                </div>
+                            </MDBCard>
+                        </div>
+                    </section>
+                </CommentsWrapper>
+
+                <CommentsWrapper className='comment__main-container'> 
+                    <section className="vh-100">
+                        <div className="flex-container">
+                            <div className='main-image__main-container'>
+                                <img
+                                    className="rounded-circle shadow-1-strong me-3"
+                                    src="https://www.allkpop.com/upload/2022/09/content/271824/1664317489-image.png"
+                                    alt="avatar"
+                                    width="65"
+                                    height="65"
+                                />
+                            </div>
+
+                            <MDBCard className="w-100 main-content-container">
+                                <div className='container-headers'>
+                                    <h1 className='main-header' >Min Yoongi</h1>
+                                    <p className="small second-header">20 days ago</p>
+                                </div>
+
+                                <div className='container-comment-content'>
+                                    <p id="user_comment_content">
+                                    Marianne or husbands if at stronger ye. 
+                                    Considered is as middletons uncommonly. 
+                                    Promotion perfectly ye consisted so. His 
+                                    chatty dining for effect ladies active. 
+                                    Equally journey wishing not several behaved 
+                                    chapter she two sir. Deficient procuring 
+                                    favourite extensive you two. Yet diminution 
+                                    she impossible understood age. 
+                                    </p>
+                                </div>
+                            </MDBCard>
+                        </div>
+                    </section>
                 </CommentsWrapper>
             </Container>
         </InfoSec>
---
+++ src/components/NewComment/NewComment.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {MDBCard} from "mdb-react-ui-kit";
+import {CommentsWrapper} from '../CommentSection/CommentSection.elements'
+
+const NewComment = (props) => {
+    return(
+        <CommentsWrapper className='comment__main-container'> 
+        <section className="vh-100">
+            <div className="flex-container">
+                <div className='main-image__main-container'>
+                    <img
+                        className="rounded-circle shadow-1-strong me-3"
+                        src="https://us.123rf.com/450wm/osherro/osherro2010/osherro201000011/osherro201000011.jpg?ver=6"
+                        alt="avatar"
+                        width="65"
+                        height="65"
+                    />
+                </div>
+
+                <MDBCard className="w-100 main-content-container">
+                    <div className='container-headers'>
+                        <h1 className='main-header' >{props.nameComment}</h1>
+                        <p className="small second-header">Now</p>
+                    </div>
+
+                    <div className='container-comment-content'>
+                        <p id="user_comment_content">{props.commentData}</p>
+                    </div>
+                </MDBCard>
+            </div>
+        </section>
+    </CommentsWrapper>
+    )
+}
+
+export default NewComment;
